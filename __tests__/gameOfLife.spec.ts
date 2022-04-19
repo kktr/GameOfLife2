f9cee53b6d190a503822d7f64907b64f9d91556f
Krystian Kat <krystian.kat@protonmail.com>
2022-04-20 00:01:26 +0200

    feat: implement all logic for gameOfLife.tick()

--- __tests__/gameOfLife.spec.ts
+++ __tests__/gameOfLife.spec.ts
@@ -1,3 +1,5 @@
+import { INumberOfNeighbors } from '../interfaces/INumberOfNeighbors';
+import { Cell } from '../src/cell';
 import { IBoard } from './../interfaces/IBoard';
 
 describe('GameOfLife', () => {
@@ -36,6 +38,24 @@ describe('GameOfLife', () => {
       [0, 0, 0],
     ]);
   });
+
+  it('cell with 2 neighbors should survived and cell with 3 neighbors should be born', () => {
+    //given
+    const board: IBoard = [
+      [1, 1, 1],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    //when
+    const gameOfLife = new GameOfLife(board);
+
+    //then
+    expect(gameOfLife.tick().getBoard()).toEqual([
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]);
+  });
 });
 
 class GameOfLife {
@@ -46,11 +66,31 @@ class GameOfLife {
   }
 
   public tick() {
-    this.board = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ];
+    const newBoard = this.board.map((row, rowIndex) => {
+      return row.map((cell, cellIndex) => {
+        const neighbors = this.getNeighbors(this.board, rowIndex, cellIndex);
+        return Cell.state(cell, neighbors as INumberOfNeighbors);
+      });
+    });
+    this.board = newBoard;
     return this;
+  }
+  private getNeighbors(board: IBoard, rowIndex: number, cellIndex: number) {
+    const neighbor = (row, cell) => {
+      return (
+        (board[rowIndex + row] && board[rowIndex + row][cellIndex + cell]) || 0
+      );
+    };
+
+    return (
+      neighbor(1, 0) +
+      neighbor(-1, 0) +
+      neighbor(0, 1) +
+      neighbor(0, -1) +
+      neighbor(1, 1) +
+      neighbor(-1, -1) +
+      neighbor(1, -1) +
+      neighbor(-1, 1)
+    );
   }
 }
