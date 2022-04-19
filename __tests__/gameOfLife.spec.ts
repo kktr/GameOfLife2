a1d353bab93cbaf13cd4e00dc42657f1ce21225f
Krystian Kat <krystian.kat@protonmail.com>
2022-04-19 23:14:16 +0200

    feat: implement return array of dead cells, when 1 cell

--- __tests__/gameOfLife.spec.ts
+++ __tests__/gameOfLife.spec.ts
@@ -18,6 +18,24 @@ describe('GameOfLife', () => {
       [0, 0, 0],
     ]);
   });
+
+  it('should return array of dead cells, when array with 1 cell is given', () => {
+    //given
+    const board: IBoard = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+    //when
+    const gameOfLife = new GameOfLife(board);
+
+    //then
+    expect(gameOfLife.tick().getBoard()).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+  });
 });
 
 class GameOfLife {
@@ -28,6 +46,11 @@ class GameOfLife {
   }
 
   public tick() {
+    this.board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
     return this;
   }
 }
